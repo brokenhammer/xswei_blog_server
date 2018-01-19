56e90a60168885152d42b0fdd99be6053cbaafab
brokenhammer <415516853@qq.com>
2018-01-19 03:31:02 +0000

    add zip blogs

--- blog_server/view.py
+++ blog_server/view.py
@@ -1,6 +1,6 @@
 from blog_server import app
-from flask import render_template,redirect,url_for,abort
-import urllib.parse
+from flask import render_template,redirect,url_for,abort,send_file,send_from_directory
+import urllib.parse,os
 from blog_server.utils import legal_path, full_path, parse_path
 
 
@@ -35,3 +35,16 @@ def blog_view(fpath,md_type):
     y, m, d, title = parse_path(fpath, md_type)
     return render_template('blog_view.html',fpath=fpath, md_type=md_type,
                            title=title)
+
+@app.route("/zip_blogs")
+def zip_blogs():
+    import tempfile
+    import zipfile
+    #tmp = tempfile.mktemp()
+    #tmp += '.zip'
+    tmp = 'blogs.zip'
+    with zipfile.ZipFile(tmp,'w') as zf:
+        for dirpath, dirnames, filenames in os.walk('data_dir/published'):
+            for fname in filenames:
+                zf.write(os.path.join(dirpath,fname))
+    return send_from_directory(os.getcwd(),tmp)
